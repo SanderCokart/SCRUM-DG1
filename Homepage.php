6785ab3f5d63c24b5656067d08adf40bbf52c6ab
Sander Cokart <112039@student.drenthecollege.nl>
2018-11-26 11:42:32 +0100

    Created a simple homepage with tabs to different themes.

--- Homepage.php
+++ Homepage.php
@@ -2,10 +2,34 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="Stylesheet.css">
   </head>
   <body>
-    <h1>Homepage</h1>
-    <p>Welcome to the Homepage!</p>
+
+    <div class="banner">
+      <h1>Forum ict</h1>
+    </div>
+
+    <div class="nav">
+      <ul class="nav">
+        <li class="nav"><a class="nav" href="Home.html">Home</a></li>
+        <li class="nav"><a class="nav" href="Gaming.html">Gaming</a></li>
+        <li class="nav"><a class="nav" href="Technology.html">Technology</a></li>
+        <li class="nav"><a class="nav" href="Content Creation.html">Content Creation</a></li>
+        <li class="nav"><a class="nav" href="Cars.html">Cars</a></li>
+        <li class="nav"><a class="nav" href="Hobbies.html">Hobbies</a></li>
+      </ul>
+    </div>
+
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
   </body>
 </html>
