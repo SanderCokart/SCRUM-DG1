19a66036812bbb8abb8ad75199bf22db12f45a99
Sander Cokart <112039@student.drenthecollege.nl>
2018-11-26 10:41:47 +0100

    Start of the homepage and therefor the website!

---
+++ Homepage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Homepage</h1>
+    <p>Welcome to the Homepage!</p>
+  </body>
+</html>
