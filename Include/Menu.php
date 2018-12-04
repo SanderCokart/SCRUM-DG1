4582a0fc937e589814a73e103d8b0d4025e75f9a
Sander Cokart <112039@student.drenthecollege.nl>
2018-12-04 10:54:26 +0100

    Nothing changed

--- Include/Menu.php
+++ Include/Menu.php
@@ -1,13 +1,13 @@
 <center>
   <div class="nav">
-    <ul class="nav">
+    <ul>
       <?php
-      $tabs = array("Home"=>"Homepage.php",
-                    "Gaming"=>"Gaming.php",
-                    "Technology"=>"Technology.php",
-                    "Content Creation"=>"Content Creation.php",
-                    "Cars"=>"Cars.php",
-                    "Hobbies"=>"Hobbies.php");
+      $tabs = array("Home"=>"/Opdrachten/SCRUM Assignment 1/Homepage.php",
+                    "Gaming"=>"/Opdrachten/SCRUM Assignment 1/Gaming.php",
+                    "Technology"=>"/Opdrachten/SCRUM Assignment 1/Technology.php",
+                    "Content Creation"=>"/Opdrachten/SCRUM Assignment 1/Content Creation.php",
+                    "Cars"=>"/Opdrachten/SCRUM Assignment 1/Cars.php",
+                    "Hobbies"=>"/Opdrachten/SCRUM Assignment 1/Hobbies.php");
 
       foreach ($tabs as $key => $value) {
         echo ("<li class=\"nav\"><a class=\"nav\" href=\"".$value."\">".$key."</a></li>");
