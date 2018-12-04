fd630ea2cd1eaae780861e243c2896226b3fddd7
Sander Cokart <112039@student.drenthecollege.nl>
2018-12-04 10:51:26 +0100

    Created the cars section with 2 brand and more to add

---
+++ Cars/Audi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <link rel="stylesheet" href="../Stylesheet.css">
+    <meta charset="utf-8">
+    <title>Cars</title>
+</head>
+
+<body>
+
+    <div class="Header"></div>
+
+    <?php include("include/Menu.php");?>
+
+</body>
+
+</html>
