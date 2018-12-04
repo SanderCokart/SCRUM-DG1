4825e78ab1a43c97e80d827bc86b179db5ae74ce
Sander Cokart <112039@student.drenthecollege.nl>
2018-12-04 10:53:59 +0100

    moved a few things

--- Homepage.php
+++ Homepage.php
@@ -1,29 +1,24 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Homepage</title>
     <link rel="stylesheet" href="Stylesheet.css">
-  </head>
-  <body>
+</head>
 
-<div class="title">
-  <h1>Forum ICT</h1>
-</div>
+<body>
 
-<?php include("include/Menu.php");?>
+    <div class="Header">
+      <a href="Account.php"><img id="accounticon" src="https://www.materialui.co/materialIcons/action/account_circle_white_192x192.png" alt="Account">
+    </div>
 
-  <div class="textbox">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-  </div>
+    <?php include("include/Menu.php");?>
 
-  <div class="textbox">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-  </div>
+    <div class="title">
+        <h1>Forum ICT</h1>
+    </div>
 
-  </body>
+</body>
+
 </html>
