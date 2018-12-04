96856da70b580232b86c9d4830674e5737f3ddc9
Sander Cokart <112039@student.drenthecollege.nl>
2018-12-04 10:54:44 +0100

    Created a hobbies section

---
+++ Hobbies.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <link rel="stylesheet" href="Stylesheet.css">
+    <meta charset="utf-8">
+    <title>Hobbies</title>
+</head>
+
+<body>
+
+    <div class="Header">
+      <a href="Account.php"><img id="accounticon" src="https://www.materialui.co/materialIcons/action/account_circle_white_192x192.png" alt="Account">
+      <a href="Homepage.php"><img class="back" src="https://lh3.ggpht.com/sqPcTNRtM6vc4pzWXWXIR6KxnRwbIz4LIYKvqwr1l__gPYkx24sV4Ak1oc2sN99dVA=s180" width="100px" alt="">
+    </div>
+
+<div class="nav">
+  <ul>
+    <li class="nav"><a class="nav" href="Hobbies/Fishing.php">Fishing</a></li>
+    <li class="nav"><a class="nav" href="Hobbies/Gaming.php">Gaming</a></li>
+    <li class="nav"><a class="nav" href="New Topic.php">New Topic</a></li>
+  </ul>
+</div>
+
+<div class="title">
+  <h1>Select A Topic</h1>
+</div>
+
+</body>
+
+</html>
---
+++ Hobbies/Fishing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="../Stylesheet.css">
+    <meta charset="utf-8">
+    <title>Fishing</title>
+  </head>
+  <body>
+
+    <div class="Header">
+      <a href="../Account.php"><img id="accounticon" src="https://www.materialui.co/materialIcons/action/account_circle_white_192x192.png" alt="Account">
+      <a href="../Homepage.php"><img class="back" src="https://lh3.ggpht.com/sqPcTNRtM6vc4pzWXWXIR6KxnRwbIz4LIYKvqwr1l__gPYkx24sV4Ak1oc2sN99dVA=s180" width="100px" alt="">
+    </div>
+
+<div class="nav">
+  <ul>
+    <li class="nav"><a class="nav" href="Fishing.php">Fishing</a></li>
+    <li class="nav"><a class="nav" href="Gaming.php">Gaming</a></li>
+    <li class="nav"><a class="nav" href="../New Topic.php">New Topic</a></li>
+  </ul>
+</div>
+
+    <?php include("../include/TPH.php");?>
+    <?php include("../include/TPH.php");?>
+
+  </body>
+</html>
